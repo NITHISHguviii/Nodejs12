8553608604e5f25fd0bd973308dc41ee67431f02
NITHISHguviii <nithishkumarv0612@gmail.com>
2023-04-24 11:51:22 +0530

    auto assign port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-
 dotenv.config();
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 //const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
 let clientdb;
